53a63116c9c40a66b18e48670ce99c5e6e9bd789
alexcapoo <alexcapoo1@gmail.com>
2022-03-28 23:51:00 -0300

    projeto completo

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Alex Mota'
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html')
